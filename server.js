eaac28ad28092d725cb596266a4b11de1c208b69
Javier <javiergdh26@gmail.com>
2026-04-21 16:14:44 +0200

    Primeras paginas web y añadidos cursos

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('.')); // Añadido para servir archivos HTML y CSS desde el directorio raíz
 
 app.get('/api/test', (req, res) => {
   res.json({ mensaje: "Backend funcionando" });
