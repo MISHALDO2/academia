442a41feb82192090f51c64db8324c1baf273ba1
Javier <javiergdh26@gmail.com>
2026-04-30 08:58:19 +0200

    Endpoint inicio de sesion con nombre

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,30 @@ const db = require('../database');
 
 // REGISTER
 router.post('/register', (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ error: "Faltan datos" });
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "Faltan datos (nombre, email o contraseña)" });
   }
 
-  const query = `INSERT INTO users (email, password, role) VALUES (?, ?, ?)`;
+  if (password.length < 4) {
+    return res.status(400).json({ error: "La contraseña es muy corta" });
+  }
+  const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
 
-  db.run(query, [email, password, 'user'], function (err) {
+  db.run(query, [name, email, password], function (err) {
     if (err) {
+      if (err.message.includes("UNIQUE constraint failed")) {
+        return res.status(400).json({ error: "El email ya está registrado" });
+      }
       return res.status(500).json({ error: "Error al registrar usuario" });
     }
 
     res.json({
-      mensaje: "Usuario registrado",
+      mensaje: "Usuario registrado correctamente",
       id: this.lastID,
-      email,
-      role: 'user'
+      nombre: name,
+      email
     });
   });
 });
